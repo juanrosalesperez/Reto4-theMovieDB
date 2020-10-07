576be026f752e7e32864ae6f3c8b031f8570a9e9
juanrosalesperez <rosalesjuanantonio@gmail.com>
2020-10-07 10:45:48 +0200

    Cambio en Actions / Series

--- src/actions/searchActions.js
+++ src/actions/searchActions.js
@@ -37,3 +37,38 @@ export const fetchMovie = (id) => (dispatch) => {
     )
     .catch((err) => console.log(err));
 };
+
+export const searchSerie = (text) => (dispatch) => {
+  dispatch({
+    type: CONSTANTS.SEARCH_SERIE,
+    payload: text,
+  });
+};
+
+export const fetchSeries = (text) => (dispatch) => {
+  axios
+    .get(
+      `https://api.themoviedb.org/3/search/tv?api_key=${APIKey}&language=es-ES&query=${text}&page=1&include_adult=false`
+    )
+    .then((response) =>
+      dispatch({
+        type: CONSTANTS.FETCH_SERIES,
+        payload: response.data,
+      })
+    )
+    .catch((err) => console.log(err));
+};
+
+export const fetchSerie = (tv_id) => (dispatch) => {
+  axios
+    .get(
+      `https://api.themoviedb.org/3/tv/${tv_id}?api_key=${APIKey}&language=es-ES`
+    )
+    .then((response) =>
+      dispatch({
+        type: CONSTANTS.FETCH_SERIE,
+        payload: response.data,
+      })
+    )
+    .catch((err) => console.log(err));
+};
